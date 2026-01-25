80bda967cf8f8f94649077efe19004d4c5e28055
Phạm Đình Thương <thuonglop94dhp@gmail.com>
2026-01-25 09:07:24 +0700

    nestjs basic

---
+++ src/users/users.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './schemas/user.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import mongoose, { Model } from 'mongoose';
+import bcrypt from "bcryptjs";
+@Injectable()
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+  hashPassword = (password: string) => {
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(password, salt);
+    return hash;
+  }
+  async create(data: CreateUserDto) {
+    const hashPassword = this.hashPassword(data.password);
+    let user = this.userModel.create({
+      email: data.email,
+      password: hashPassword,
+      name: data.name
+    })
+    return user;
+  }
+
+  findAll() {
+    return `This action returns all users`;
+  }
+
+  async findOne(id: string) {
+    if(!mongoose.Types.ObjectId.isValid(id)) return "không tìm thấy user này";
+    return await this.userModel.findOne({
+      _id: id
+    })
+  }
+
+  async update(id: string, updateUserDto: UpdateUserDto) {
+    return await this.userModel.updateOne({_id: id}, {...updateUserDto});
+  }
+
+  async remove(id: string) {
+    if(!mongoose.Types.ObjectId.isValid(id)) return "không tìm thấy user này";
+    return await this.userModel.deleteOne({_id: id});
+  }
+}
