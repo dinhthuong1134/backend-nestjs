779a4e41887141d8027cf9c6fd1d5723c79a5b96
Phạm Đình Thương <thuonglop94dhp@gmail.com>
2026-01-25 22:06:58 +0700

    commit

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@n
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { AuthGuard } from './users.guard';
+
 
 @Controller('users')
 export class UsersController {
@@ -13,7 +13,6 @@ export class UsersController {
     return this.usersService.create(CreateUserDto);
   }
 
-  @UseGuards(AuthGuard)
   @Get()
   findAll() {
     return this.usersService.findAll();
